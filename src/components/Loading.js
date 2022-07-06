cc9f745db6dc636f315c5a6227704a2a77492174
Deva_kumar.N <ndeva96@gmail.com>
2022-07-06 11:13:43 -0900

    completed

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const loading = () => {
+const Loading = () => {
   return (
-    <div className='loading'>
+    <div className='Loading'>
         <h1>
             Loading...
         </h1>
@@ -10,4 +10,4 @@ const loading = () => {
   )
 }
 
-export default loading
+export default Loading
